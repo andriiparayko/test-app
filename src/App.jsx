9d17243d58df5f5be2a601921cbf4276c6f79903
Andrii Parayko <121669263+andriiparayko@users.noreply.github.com>
2025-07-28 00:35:38 +0300

    [fix] routing config

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout.jsx";
 import Users from "./features/users";
 import About from "./features/about";
 
 const App = () => (
-  <Routes>
-    <Route
-      path=""
-      element={<Layout />}
-      errorElement={<h2>Something went wrong. Please try again later.</h2>}
-    >
-      <Route path="/" element={<Navigate to="/about" replace />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/users/*" element={<Users />} />
+  <BrowserRouter basename="/test-app">
+    <Routes>
+      <Route
+        path=""
+        element={<Layout />}
+        errorElement={<h2>Something went wrong. Please try again later.</h2>}
+      >
+        <Route path="/" element={<Navigate to="/about" replace />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users/*" element={<Users />} />
+        <Route path="*" element={<h2>404 - Page Not Found</h2>} />
+      </Route>
       <Route path="*" element={<h2>404 - Page Not Found</h2>} />
-    </Route>
-  </Routes>
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
