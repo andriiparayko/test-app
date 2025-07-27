6e02c8613d46f8835e23a21a3a808fb9f7600c8c
Andrii Parayko <121669263+andriiparayko@users.noreply.github.com>
2025-07-28 01:02:09 +0300

    [fix] fixed menu active item

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,25 +1,37 @@
 import { Layout as AntLayout, Menu } from "antd";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 import { MENU_ITEM_LIST } from "./constants.js";
+import { getCurrentPath } from "./utils.js";
 
 const { Header, Content } = AntLayout;
 
-const Layout = () => (
-  <AntLayout>
-    <Header>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[MENU_ITEM_LIST[0].key]}>
-        {MENU_ITEM_LIST.map((item) => (
-          <Menu.Item key={item.key}>
-            <Link to={item.path}>{item.label}</Link>
-          </Menu.Item>
-        ))}
-      </Menu>
-    </Header>
-    <Content style={{ padding: "24px" }}>
-      <Outlet />
-    </Content>
-  </AntLayout>
-);
+const Layout = () => {
+  const location = useLocation();
+
+  const currentPath = getCurrentPath(location);
+
+  return (
+    <AntLayout>
+      <Header>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={[MENU_ITEM_LIST[0].key]}
+          selectedKeys={[currentPath]}
+        >
+          {MENU_ITEM_LIST.map((item) => (
+            <Menu.Item key={item.key}>
+              <Link to={item.path}>{item.label}</Link>
+            </Menu.Item>
+          ))}
+        </Menu>
+      </Header>
+      <Content style={{ padding: "24px" }}>
+        <Outlet />
+      </Content>
+    </AntLayout>
+  );
+};
 
 export default Layout;
